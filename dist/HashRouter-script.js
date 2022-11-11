e396036c6ad1186198fd7ddd1ac571c46f9e2ea5
Abdul Habra <abdul.habra@gmail.com>
2022-11-10 21:13:37 -0500

    generate dist

--- dist/HashRouter-script.js
+++ dist/HashRouter-script.js
@@ -1,6 +1,6 @@
 // HashRouter.js Library for hash-based routing.
 // https://github.com/ahabra/hash-router
-// Copyright 2021 (C) Abdul Habra. Version 0.1.7.
+// Copyright 2021 (C) Abdul Habra. Version 0.2.0.
 // Apache License Version 2.0
 
 
